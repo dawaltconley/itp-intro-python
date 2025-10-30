7f016d43af0e67e57f1ccb8d4c564f288a740652
Dylan Awalt-Conley <web@dylan.ac>
2025-10-30 16:30:33 -0400

    remove answers

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,5 @@
 # fizzbuzz.py
 i = 1
 while i <= 100:
-    answer = ''
-    if i % 3 == 0:
-        answer = answer + 'Fizz'
-    if i % 5 == 0:
-        answer = answer + 'Buzz'
-    if len(answer) == 0:
-        answer = i
-    print(answer)
+    ... # solve fizzbuzz for i
     i = i + 1
--- lists.py
+++ lists.py
@@ -6,19 +6,14 @@ numbers = [4, 2, 9, 1, 3]
 # loop over the list, printing each item
 # replace the elipses in this file with your own code
 
-i = 0
-while i < len(numbers):
-    n = numbers[i]
-    print(n)
-    i = i + 1
+...
 
 # what is their sum? add them together by looping over the list
 # then print the sum
 
 sum = 0
 
-for n in numbers:
-    sum = sum + n
+...
 
 print(sum)
 
@@ -27,9 +22,7 @@ print(sum)
 
 max = 0
 
-for n in numbers:
-    if n > max:
-        max = n
+...
 
 print(max)
 
@@ -38,25 +31,19 @@ print(max)
 
 min = 0
 
-for n in numbers:
-    if n < min:
-        min = n
+...
 
 print(min)
 
 # loop over this list of numbers and add them to a new array (unique)
-# so that only unique contains only the
-# this is quite a bit harder
+# so that only unique contains only the unique values
+# you can add to an array using the `append` method, e.g.
+#   unique.append(4)
+# (this is quite a bit harder)
 
 duplicates = [4, 2, 9, 2, 3, 3, 4, 6, 8, 6, 6, 0, 10, 1, 4]
 unique = []
 
-for n in duplicates:
-    is_unique = True
-    for u in unique:
-        if n == u:
-            is_unique = False
-    if is_unique:
-        unique.append(n)
+...
 
 print(unique)
